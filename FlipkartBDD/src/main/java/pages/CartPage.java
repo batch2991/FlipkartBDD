5789e192dd4aea315ef41ad529d3fb4c14ff378f
venkatg <venkatgn.gutta@gmail.com>
2023-04-19 11:33:03 +0530

    cartpage updated again and trackorder page created

--- FlipkartBDD/src/main/java/pages/CartPage.java
+++ FlipkartBDD/src/main/java/pages/CartPage.java
@@ -10,6 +10,7 @@ public class CartPage
   public void removefromcart()
   {
 	  System.out.println("to remove a product from cart");
+	  System.out.println("and return the list of products in cart");
   }
   public void validateproductsinCart()
   {
